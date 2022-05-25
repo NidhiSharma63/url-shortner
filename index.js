b5cb32b878ded4a5a3d71e72abffd7838b8c19bd
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-05-25 22:13:26 +0100

    show-short-link function created

--- index.js
+++ index.js
@@ -19,29 +19,7 @@ shortenBtn.addEventListener('click',()=>{
         // set value in local storage
         shortlinkArray.push(data.result.short_link);
         localStorage.setItem(`${inputLink.value}`,JSON.stringify(shortlinkArray));
-        let result ='';
-        // iterate on local Storage keys and values
-        for(let i=0;i<localStorage.length;i++){
-          const output = 
-          `
-          <div class="link-container">
-          <div class="original-link">
-              ${localStorage.key(i)}
-              </div>
-            <div class="short-link">
-              <div class="last-col">
-              ${localStorage.getItem(localStorage.key(i))}
-              </div>
-              <div class="copy-btn">
-              <button class="copy">copy</button>
-              </div>
-              </div>
-              
-              </div>
-              `;
-              result+=output;
-        }
-        shortlinkContainer.innerHTML = result;
+        showShortlink();
         inputLink.value='';
       });
 });
@@ -50,3 +28,33 @@ const shortenlink= async(value)=>{
     const res = await data.json();
     return res;
 };
+
+let showShortlink = ()=>{
+  let result ='';
+  // iterate on local Storage keys and values
+  for(let i=0;i<localStorage.length;i++){
+    const output = 
+    `
+    <div class="link-container">
+    <div class="original-link">
+        ${localStorage.key(i)}
+        </div>
+      <div class="short-link">
+        <div class="last-col">
+        ${localStorage.getItem(localStorage.key(i))}
+        </div>
+        <div class="copy-btn">
+        <button class="copy">copy</button>
+        </div>
+        </div>
+        
+        </div>
+        `;
+        result+=output;
+  }
+  shortlinkContainer.innerHTML = result;
+}
+
+window.onload=()=>{
+  showShortlink()
+}
